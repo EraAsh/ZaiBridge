573eb2a20593c05c80265edefcf71e11d42412bb
Futureppo <luominzhi2005@qq.com>
2025-12-12 20:45:41 +0800

    重构中

--- app/db/redis.py
+++
@@ -1,7 +0,0 @@
-import redis.asyncio as redis
-from app.core.config import settings
-
-pool = redis.ConnectionPool.from_url(settings.REDIS_URL, decode_responses=True)
-
-async def get_redis():
-    return redis.Redis(connection_pool=pool)
